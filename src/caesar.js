9b9e4af57bd84bff65281c07835b92103d4a60db
Ranger-Jay <68936288+Ranger-Jay@users.noreply.github.com>
2021-04-22 12:25:31 -0400

    Add files via upload

--- src/caesar.js
+++ src/caesar.js
@@ -1,84 +1,86 @@
+
 // Please refrain from tampering with the setup code provided here,
 // as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope of the anonymous function on line 6
-
-// =========TO RESTART=====================
-
-// const caesarModule = (function () {
-//   // you can add any code you want within this function scope
-
-//   function caesar(input, shift, encode = true) {
-//     // your solution code here
-//   }
-
-//   return {
-//     caesar,
-//   };
-// })();
-// =========TO RESTART ABOVE=====================
+// Only add code (e.g., helper methods, variables, etc.) within the scope
+// of the anonymous function on line 6
 
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-function caesar(input, shift, encode = true) {
-    // your solution code here
+  function caesar(input, shift, encode = true) {
+    let inputs = input.toLowerCase();
+    let result = [];
+    let inputLetters = [];
     
-let output = "";
-let shiftValue = 0;
-
-    // THIS CHECKS FOR BAD SHIFT
-if(!shift || shift > 25 || shift < -25) {
-     console.log(`shift: ${shift}`);
-     return false;
-    } //  END IF 30
-
-  // LOOP THROUGH AND CONVERT INPUT TO LOWER CASE
-    for (let i = 0; i < input.length; i++) {
-      let inputCode = input.toLowerCase().charCodeAt(i);
-      let inputCharacter = input[i];
-
-      // +/- shift depending on encode is true or false
-      encode === true ? shiftValue = inputCode + shift : shiftValue = inputCode - shift;
-
-
-      if (encode){
-        // upper: A = 65, Z = 90
-        if(inputCode >= 65 && inputCode <= 90) {
-          inputCharacter = String.fromCharCode(((shiftValue - 65 ) % 26) + 65);
-          } // end if for upper
-  
-          // lower: a = 97, z = 122
-          if(inputCode >= 97 && inputCode <= 122) {
-            inputCharacter = String.fromCharCode(((shiftValue - 97 ) % 26) + 97);
-          } // end if for lower
-
-      } // end outer if
-      if(!encode){
-        let temp = inputCode - 97 - shift;  // results in a negatv?
-
-        if( shiftValue >= 65 && shiftValue <= 122) { 
-          inputCharacter = String.fromCharCode(shiftValue); 
-        } //end if
-        
-        if (temp >= 0){    // forr cases temp >=0
-          inputCharacter = String.fromCharCode(temp % 26 + 97);
-        } else if (inputCode < 65 ){
-          inputCharacter = String.fromCharCode(inputCode); 
-        } else 
-        {
-          inputCharacter = String.fromCharCode (26 + temp + 97); 
-        }
-      } // end if
-
-      // view 
-      output += inputCharacter;
-    } // end for
-    console.log(`input:  ${input}`);
-    console.log(`output: ${output}`);
-    return output;
+    if (shift === 0 || shift < -25 || shift > 25 || shift === null || shift === "" || shift === false) {
+      return false; // handles if shift is within the parameters
+    }else if(encode === true && shift < 0){
+      shiftLeft(inputs, shift, inputLetters, alphabet, result); //handles negative shift on encode
+    }else if(encode === true && shift > 0){
+      shiftRight(inputs, shift, inputLetters, alphabet, result); // handles positive shift on encode
+    }
+    else if(encode === false && shift < 0){
+      shiftRight(inputs, Math.abs(shift), inputLetters, alphabet, result); // handles negative shift on decode
+    }
+    else if (encode === false && shift > 0){
+      shiftLeft(inputs, -Math.abs(shift), inputLetters, alphabet, result); //handles positive shift on decode
+    }
+    return result.join("");
   }
-
-  return {caesar};
+  
+  
+  function shiftLeft(inputs, shift, inputLetters, alphabet, result){
+   
+    for (let i=0; i<inputs.length; i++){
+      inputLetters.push(inputs[i]);
+    }
+  
+    for (let i=0; i<inputLetters.length; i++){
+      if (!alphabet.includes(inputLetters[i])){
+        result.push(inputLetters[i]);
+      }
+      for (let j=0; j<alphabet.length; j++){
+        if (inputLetters[i] === alphabet[j]) {
+          if ((alphabet.indexOf(alphabet[j]) + shift) < 0){
+            dif = (alphabet.indexOf(alphabet[j]) + shift);
+            result.push(alphabet[alphabet.length + dif]);
+          }else{
+            result.push(alphabet[j+shift]);
+          }
+        }
+      }
+    } 
+    return result;
+  }
+  
+  function shiftRight(inputs, shift, inputLetters, alphabet, result){
+    
+    for (let i=0; i<inputs.length; i++){
+      inputLetters.push(inputs[i]);
+    }
+  
+    for (let i=0; i<inputLetters.length; i++){
+      if (!alphabet.includes(inputLetters[i])){
+        result.push(inputLetters[i]);
+      }
+      for (let j=0; j<alphabet.length; j++){
+        if (inputLetters[i] === alphabet[j]) {
+          if ((alphabet.indexOf(alphabet[j]) + shift) >= alphabet.length){
+            dif = (alphabet.length - alphabet.indexOf(alphabet[j]));
+            
+            result.push(alphabet[0 + shift - dif]);
+          }else{
+            result.push(alphabet[j+shift]);
+          }
+        }
+      }
+    }
+    return result;  
+  }
+  
+  return {
+    caesar,
+  };
 })();
 
 module.exports = caesarModule.caesar;
