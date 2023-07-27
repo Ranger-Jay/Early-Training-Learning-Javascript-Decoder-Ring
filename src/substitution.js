7a2df9d65dd6571c1b23d81447efde5d135f3cbd
Jay <careerforjay@gmail.com>
2023-07-26 20:34:13 -0400

    Update substitution.js

--- src/substitution.js
+++ src/substitution.js
@@ -1,9 +1,4 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
 
   function inputChecker(alphabet){
     switch(true){
